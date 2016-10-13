50ba740b0e59402c3eebcb1468c6bc38b2200033
Jake Obron <jbobron@gmail.com>
2016-10-12 22:39:55 -0700

    cleans up index.js, adds path for path.join(), adds login from index.ejs

--- index.js
+++ index.js
@@ -1,45 +1,26 @@
+const path = require('path');
 const express = require('express');
 const passport = require('passport');
-const http = require('http');
-const StravaStrategy = require('passport-strava-oauth2').Strategy;
-const util = require('util');
-const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const StravaStrategy = require('passport-strava-oauth2').Strategy;
+
 const STRAVA_CLIENT_ID = process.env.STRAVA_CLIENT_ID;
 const STRAVA_CLIENT_SECRET = process.env.STRAVA_CLIENT_SECRET;
 
 const app = express();
 
-// configure Express
-var cbUrl = '';
-if (app.get('env') === 'development') {
-  // var use = app.use(express.errorHandler());
-  cbUrl = 'http://127.0.0.1:3000/auth/strava/callback';
-  // DEV NEEDS THIS
-} else {
-  cbUrl = 'http://strava-pathlete.herokuapp.com/auth/strava/callback';
-}
-app.use(express.session({ secret: 'keyboard cat', cookie: { maxAge: 1000 * 60 * 60 } }));
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.  However, since this example does not
-//   have a database of user records, the complete Strava profile is
-//   serialized and deserialized.
-passport.serializeUser((user, done) => done(null, user));
+var cbUrl = app.get('env') === 'development' ?
+  'http://127.0.0.1:3000' :
+  'http://strava-pathlete.herokuapp.com';
 
+passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((obj, done) => done(null, obj));
 
-
-// Use the StravaStrategy within Passport.
-//   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an accessToken, refreshToken, and Strava
-//   profile), and invoke a callback with a user object.
 passport.use(new StravaStrategy({
   clientID: STRAVA_CLIENT_ID,
   clientSecret: STRAVA_CLIENT_SECRET,
-  callbackURL: cbUrl,
+  callbackURL: cbUrl + '/auth/strava/callback',
 },
   (accessToken, refreshToken, profile, done) => {
     // asynchronous verification, for effect...
@@ -55,31 +36,23 @@ passport.use(new StravaStrategy({
   }
 ));
 
-
-// app.configure(() => {
-app.set('views', __dirname + '/public');
+app.set('views', path.join(__dirname, '/public'));
 app.set('view engine', 'ejs');
+app.use(express.session({ secret: 'keyboard cat', cookie: { maxAge: 1000 * 60 * 60 } }));
 app.use(express.logger());
 app.use(express.cookieParser());
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.json());
 app.use(methodOverride());
-
-// below causes memory leak on heroku prod, use connect-mongo or cookie-Session
-
-
-// Initialize Passport!  Also use passport.session() middleware, to support
-// persistent login sessions (recommended).
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(app.router);
-app.use(express.static(__dirname + '/public'));
-// });
 
 // Simple route middleware to ensure user is authenticated.
 //   Use this route middleware on any resource that needs to be protected.  If
 //   the request is authenticated (typically via a persistent login session),
 //   the request will proceed.  Otherwise, the user will be redirected to the
-//   login page.
+//   login page. (ex: see /account route)
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
   res.redirect('/login');
@@ -95,25 +68,16 @@ app.get('/account', ensureAuthenticated, (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-  res.render('login', { user: req.user });
+  // res.render('login', { user: req.user });
+  res.redirect('auth/strava');
 });
 
-// GET /auth/strava
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in Strava authentication will involve
-//   redirecting the user to strava.com.  After authorization, Strava
-//   will redirect the user back to this application at /auth/strava/callback
+// passport.authenticate() authenticates the request w/ strava by redirecting
+// this request to strava, authorizing user, and then strava redirects back
+// to /auth/strava/callback at the Authorization Callback Domain set in Strava API settings
 app.get('/auth/strava',
-  passport.authenticate('strava', { scope: ['public'] }), (req, res) => {
-    // The request will be redirected to Strava for authentication, so this
-    // function will not be called.
-  });
+  passport.authenticate('strava', { scope: ['public'] }));
 
-// GET /auth/strava/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
 app.get('/auth/strava/callback',
   passport.authenticate('strava', { failureRedirect: '/login' }), (req, res) => {
     res.redirect('/');
@@ -124,8 +88,5 @@ app.get('/logout', (req, res) => {
   res.redirect('/');
 });
 
-if (app.get('env') === 'development') {
-  app.listen(3000);
-} else {
-  app.listen(process.env.PORT);
-}
+if (app.get('env') === 'development') app.listen(3000);
+else app.listen(process.env.PORT);
